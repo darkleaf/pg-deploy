b1585f9a5afe1ca2e8068f459887fb93266f790c
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-01-10 23:01:08 +0300

    before and after

--- index.js
+++ index.js
@@ -1,75 +1,87 @@
 "use strict";
 
+const pify = require('pify');
 const glob = require('glob');
-var pgp = require('pg-promise')(/*options*/);
+const pgp = require('pg-promise')(/*options*/);
+const fs = require('fs');
 
-function globp(pattern, options) {
-    return new Promise((resolve, reject) => {
-        glob(pattern, options, (err, files) => {
-            err ? reject(err) : resolve(files);
-        })
-    });
-}
+const MIGRATIONS_TABLE_NAME = 'pg_deploy_migrations';
 
-function promiseChain(functions, initial) {
-    if (!initial) initial = Promise.resolve();
-    return functions.reduce((previous, func) => previous.then(func), initial)
+
+//function promiseChain(functions, initial) {
+//    if (!initial) initial = Promise.resolve();
+//    return functions.reduce((previous, func) => previous.then(func), initial)
+//}
+
+function flatArray(array) {
+    return Array.prototype.concat.apply([], array);
 }
 
 function getFilePaths(patterns) {
-    return Promise
-        .all(patterns.map(pattern => globp(pattern)))
-        .then(fileGroups => Array.prototype.concat.apply([], fileGroups))
-
+    return Promise.all(patterns.map(pattern => pify(glob)(pattern)))
+        .then(flatArray);
 }
 
-function runMigration(db, filePath) {
-    return Promise.resolve();
+function getPassedMigrations(db) {
+    return db
+        .manyOrNone(`SELECT name from ${MIGRATIONS_TABLE_NAME};`)
+        .then(r => console.log(r))
 }
 
-function runMigrations(db, migrationsGlobs) {
-    return function() {
-        return db
-            .any(`SELECT name FROM migrations`)
-            .then(res => console.log(res))
-            .then(() => getFilePaths(migrationsGlobs))
-    };
-    //    .then((paths) => promiseChain(paths.map(path => runMigration(db, path))))
-}
+//function runMigration(db, filePath) {
+//    return Promise.resolve();
+//}
+//
+
+
+
 
 function initTables(db) {
-    return function() {
-        return db.query(`CREATE TABLE IF NOT EXISTS migrations (name TEXT);`);
-    }
+    return db.query(`CREATE TABLE IF NOT EXISTS ${MIGRATIONS_TABLE_NAME} (name TEXT);`);
+}
+
+
+function runScript(db, path) {
+    pify(fs.readFile)(path, 'utf8')
+        .then(content => db.query(content));
+}
+
+function runScripts(db, globs) {
+    return Promise.resolve()
+        .then(() => getFilePaths(globs))
+        .then(paths => Promise.all(paths.map(path => runScript(db, path))));
+}
+
+function runMigrations(db, globs) {
+    return Promise.all([getFilePaths(globs), getPassedMigrations(db)])
+
+        //.then(getFilePaths(globs))
+        //.then(getPassedMigrations(db))
+
+    //.then((paths) => promiseChain(paths.map(path => runMigration(db, path))))
+}
+
+function saveStructure() {
+    return Promise.resolve();
 }
 
 module.exports.deploy = function(rawOptions) {
     const defaultOptions = {
-        migrations: []
+        beforeScripts: [],
+        migrations: [],
+        afterScripts: [],
+        structurePath: ''
     };
     const options = Object.assign(defaultOptions, rawOptions);
 
 
     const db = pgp('postgres://postgres@/pg-deploy');
 
-
-
     return Promise.resolve()
-        .then(initTables(db))
-        .then(runMigrations(db, options.migrations))
-
-
-
-    //pg.connect('postgres', function(err, client, done) {
-    //    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //        call `done()` to release the client back to the pool
-            //done();
-            //console.log(result.rows[0].number);
-            //output: 1
-        //});
-    //});
-    //
-    //
-    //
-    //console.log(migrations);
+        .then(() => initTables(db))
+        .then(() => runScripts(db, options.beforeScripts))
+        .then(() => runMigrations(db, options.migrations))
+        .then(() => runScripts(db, options.afterScripts))
+        .then(() => saveStructure(db, options.structurePath))
+        .then(() => undefined)
 };
--- test/test.js
+++ test/test.js
@@ -1,19 +1,45 @@
 "use strict";
 
 const assert = require('assert');
+const pgp = require('pg-promise')();
+
+
+const db = pgp('postgres://postgres@/pg-deploy');
 
 const PgDeploy = require('../');
 
+
+function addAlreadyPerformed(db, name) {
+    return db.none("INSERT INTO pg_deploy_migrations(name) VALUES(${name})", {name})
+}
+
 describe('PgDeploy', () => {
-
-    it('ok', (done) => {
+    before(done => {
         PgDeploy
-            .deploy({migrations: ['test/migrations/**/*.sql']})
+            .deploy({
+                beforeScripts: ['test/before-scripts/**/*.sql'],
+                migrations: ['test/migrations/**/*.sql'],
+                afterScripts: ['test/after-scripts/**/*.sql']
+            })
             .then(done, done);
+    });
 
+    before(done => {
+        addAlreadyPerformed(db, 'test/migrations/0-already-performed.sql')
+            .then(done, done);
+    });
 
+    it('run before scripts', (done) => {
+        db
+            .func('before_test_func', [42])
+            .then(res => assert.equal(42, res[0].before_test_func))
+            .then(done, done);
+    });
 
-        //assert.equal(-1, [1,2,3].indexOf(5));
-        //assert.equal(-1, [1,2,3].indexOf(0));
+    it('run after scripts', (done) => {
+        db
+            .func('after_test_func', [42])
+            .then(res => assert.equal(42, res[0].after_test_func))
+            .then(done, done);
     });
 });
